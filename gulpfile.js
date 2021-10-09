f03ec660df195d43930ded246a698e89fc27c854
Alexis Evaristo López Gómez <79285054+AlexiSkyline@users.noreply.github.com>
2021-10-08 23:48:23 -0500

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require( 'gulp' );
+const sass = require( 'gulp-sass' );
+const autoprefixer = require( 'gulp-autoprefixer' );
+
+function css() {
+    return gulp
+        .src( 'scss/app.scss' )
+        .pipe( autoprefixer ( { 
+            overrideBrowsers : [ 'last 2 versions' ],
+            cascade :false
+        } ) )
+        .pipe( sass ( {
+            outputStyle : 'expanded', // nested, compact, compressed 
+        } ) )
+        .pipe( gulp.dest( 'css' ) );
+}
+function watchFiles() {
+    gulp.watch( 'scss/*.scss', css );
+    gulp.watch( 'watch' );
+}
+// Registrar funciones
+gulp.task( 'css', css );
+gulp.task( 'watch', gulp.parallel( watchFiles ) );
